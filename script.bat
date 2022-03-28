e97f732a63e88c7e2570635142e09ded9309f963
roni-info <roni_info@yahoo.com.br>
2022-03-28 19:27:20 -0300

    Aula de Revisão

---
+++ script.bat
@@ -0,0 +1,9 @@
+cd c:\exemplo
+echo "Criando Pasta ! Aguarde"
+timeout /t 5
+mkdir outros
+echo "Teste de arquivo - Serratec 2022" > arquivo6.txt
+mkdir outros-exemplos
+echo "Teste de arquivo - Serratec 2022" > arquivo7.txt
+timeout /t 10
+echo "Fim de Script !!!"
